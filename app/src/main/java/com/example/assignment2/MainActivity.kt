aa89e6a3abab2f2cdb3258dced3387550879a7c5
muhammaddaimnaveed <160724023+muhammaddaimnaveed@users.noreply.github.com>
2024-06-09 11:08:44 +0200

    Update MainActivity.kt
    The code below delays the auto transition screen by 10 seconds

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -13,15 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+The code below delays the auto transition screen by 10 seconds
 // This code delays the loading screens automatic transition to the home splash screen by 10 second.
 
-        val DELAY_TIME: Long = 10000
+
+
+
+           val DELAY_TIME: Long = 10000
         Handler().postDelayed({
             val intent = Intent(this, page1::class.java)
             startActivity(intent)
